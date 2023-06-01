323ae0965ff9fa88cd10d23c3d93e268d9413841
ChanaLSznicer <clsznicer@gmail.com>
2023-05-31 18:15:26 -0700

    class 51 c# work

---
+++ html/51/csAssessmentMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shirt_example
+{
+    class Program
+    {
+        static string[] colorOptions = { "green", "red", "pink", "blue" };
+        static string[] patternOptions = { "polka dot", "striped", "checkerd", "zig zag" };
+        //static int count  = 0; 
+        
+        static void Main(string[] args)
+        {
+            //colors colors1 = new colors();
+            //patterns patterns1 = new patterns();
+           //generateShirts();
+            generateShirts2();
+
+
+        }
+        //option #1
+        static void generateShirts()
+        {
+            int count = 0;
+          
+            foreach (var item in colorOptions)
+            {
+                foreach (var pattern in patternOptions)
+                {
+                    Console.WriteLine($"shirt{count+1}: {item} {pattern}");
+                    count++;
+                    
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"total shirts combos: {count}");
+            
+        }
+
+        static void generateShirts2()
+        {
+            int count = 0;
+            List<shirt> shirts = new List<shirt>();
+            foreach (var item in colorOptions)
+            {
+                foreach (var pattern in patternOptions)
+                {
+                    shirts.Add(new shirt(item, pattern));
+                    Console.WriteLine($"shirt{count+1}  {shirts[count]}");
+                    count++;
+                    //shirt[] shirts = new shirt[count]; //each shirt should get this info how?
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"total shirts combos: {count}");
+
+        }
+    }
+
+}
---
+++ html/51/shirt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shirt_example
+{
+    internal class shirt
+    {
+        public string color { get; set; }
+        public string pattern { get; set; }
+        public shirt(string color, string pattern)
+        {
+            this.color = color;
+            this.pattern = pattern; 
+        }
+        public override string ToString()
+        {
+            return $"{this.color}{this.pattern}";
+        }
+    }
+}
